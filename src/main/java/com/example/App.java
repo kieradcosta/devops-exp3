52bc575a0b0bc9f27f1ec8caef7cde6b95649024
kieradcosta <kieradcosta16@gmail.com>
2026-02-09 09:24:20 +0000

    sum

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    
+    int a=5;
+    int b=7;
+    int sum=a + b;
+
+    System.out.println("Sum of a+b="+sum);
     }
 }
